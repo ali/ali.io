585f304bc487c0db7d079a4ed30ca8ec3aee81d8
Ali Ukani <ali@drift.com>
2017-01-07 18:37:00 -0500

    new site

---
+++ components/text.js
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react'
+
+export const A = ({ href = '#', target = '_blank', children, ...rest }) => (
+  <a {...rest} href={href} target={target} rel='noopener noreferrer'>
+    {children}
+  </a>
+)
+
+export const P = ({ children }) => (
+  <p className='.P'>
+    {children}
+    <style jsx>
+    {`
+      .P {
+        line-height: 1.6;
+      }
+    `}
+    </style>
+  </p>
+)
